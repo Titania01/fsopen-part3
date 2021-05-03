537210879f6c24e7e92aadd603b4a820a6bd38e6
Titania01 <adegbitebidmus@gmail.com>
2021-05-03 14:39:20 +0100

    feat: update codebase

--- index.js
+++ index.js
@@ -1,92 +1,124 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+const cors = require("cors");
 
-const cors = require('cors')
+app.use(cors());
 
-app.use(cors())
+app.use(express.static("build"));
 
-app.use(express.static('build'))
+// const mongoose = require("mongoose");
 
+// const url = process.env.MONGODB_URI;
+
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+
+const Note = require("./models/note");
+
+// app.use(express.json());
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// });
+
+// noteSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
+// const Note = mongoose.model("Note", noteSchema);
 
 let notes = [
   {
     id: 1,
     content: "HTML is easy",
     date: "2019-05-30T17:30:31.098Z",
-    important: true
+    important: true,
   },
   {
     id: 2,
     content: "Browser can execute only Javascript",
     date: "2019-05-30T18:39:34.091Z",
-    important: false
+    important: false,
   },
   {
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+    important: true,
+  },
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/notes', (req, res) => {
-   res.json(notes)
-})
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
-    : 0
-  return maxId + 1
-}
-app.post('/api/notes', (req, res) => {
-  const body = req.body
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
 
-  if(!body.content) {
+  if (!body.content) {
     return res.status(400).json({
-      error: 'content missing'
-    })
+      error: "content missing",
+    });
   }
-  
+
   const note = {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
-  notes = notes.concat(note)
+    id: generateId(),
+  };
+  notes = notes.concat(note);
 
-  console.log(req.headers)
-  res.json(note)
-})
+  console.log(req.headers);
+  res.json(note);
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = parseInt(request.params.id)
-  const note = notes.find(note =>  note.id === id)
+// app.get("/api/notes/:id", (request, response) => {
+//   const id = parseInt(request.params.id);
+//   const note = notes.find((note) => note.id === id);
 
-  if(note) {
-    response.json(note)
-  }else{
-    response.status(404).end()
-  }
-  response.json(note)
-})
+//   if (note) {
+//     response.json(note);
+//   } else {
+//     response.status(404).end();
+//   }
+//   response.json(note);
+// });
 
-app.delete('/app/notes/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-  res.status(204).end()
-})
+app.delete("/app/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
+  res.status(204).end();
+});
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
